7e400e9750c48ded96ec2c1734087769aaf6fe6f
Pulakeshin <54570617+Pulakeshin@users.noreply.github.com>
2019-09-09 17:32:35 +0530

    Add files via upload

---
+++ Class Assignment1/ABCCorp/ABCCorp/Program.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ABCCorp
+{
+    class InvalidCreditLimit : Exception
+    {
+        public InvalidCreditLimit(string message) : base(message)
+        {
+
+        }
+    }
+    class Customer
+    {
+        private string _customerID;
+        private string _customerName;
+        private string _address;
+        private string _city;
+        private string _phone;
+        private int _creditLimit;
+
+        //DefaultConstructor
+        public Customer()
+        {
+            
+        }
+
+        //Parameterised Constuctor
+        public Customer(string customerID,string customerName,string address,string city,string phone,int creditLimit)
+        {
+            CustomerID = customerID;
+            CustomerName = customerName;
+            Address = address;
+            City = city;
+            Phone = phone;
+            CreditLimit = creditLimit;
+        }
+
+        //Properties
+        public string CustomerID
+        {
+            set
+            {
+                _customerID = value;
+            }
+            get
+            {
+                return _customerID;
+            }
+        }
+
+        public string CustomerName
+        {
+            set
+            {
+                _customerName = value;
+            }
+            get
+            {
+                return _customerName;
+            }
+        }
+
+        public string Address
+        {
+            set
+            {
+                _address = value;
+            }
+            get
+            {
+                return _address;
+            }
+        }
+
+        public string City
+        {
+            set
+            {
+                _city = value;
+            }
+            get
+            {
+                return _city;
+            }
+        }
+
+        public string Phone
+        {
+            set
+            {
+                _phone = value;
+            }
+            get
+            {
+                return _phone;
+            }
+        }
+
+        public int CreditLimit
+        {
+            set
+            {
+                if (value<50000)
+                {
+                    _creditLimit = value;
+                }
+                else
+                {
+                    throw new InvalidCreditLimit("Credit Limit Exceeded");
+                }
+            }
+            get
+            {
+                return _creditLimit;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                //Customer customer = new Customer("1652", "pulak", "cuttack", "7645367265", "loki", 58444);
+                Customer customer = new Customer();
+
+                Console.WriteLine("Enter Customer ID :");
+                customer.CustomerID = Console.ReadLine();
+                Console.WriteLine("Enter Customer Name :");
+                customer.CustomerName = Console.ReadLine();
+                Console.WriteLine("Enter Customer Address :");
+                customer.Address = Console.ReadLine();
+                Console.WriteLine("Enter Customer City :");
+                customer.City = Console.ReadLine();
+                Console.WriteLine("Enter Phone number :");
+                customer.Phone = Console.ReadLine();
+                Console.WriteLine("Enter Credit :");
+                customer.CreditLimit = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception ex)
+            {
+                string content = $"\n\n{DateTime.Now}" +
+                   $"\nMessage :{ex.Message}" +
+                   $"\nType :{ex.GetType().ToString()}";
+
+                Console.WriteLine(content);
+
+            }
+
+            finally
+            {
+                if ()
+                {
+
+                }
+                Console.WriteLine("\nWARNING : Credit should be less than 50000");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Class Assignment1/Array/Array/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+namespace ArrayExample
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Single dim array
+
+            int[] firstArray = new int[5] { 10, 20, 30, 40, 50 };
+            Console.WriteLine("Single dimension array with for loop");
+            for (int i = 0; i < firstArray.Length; i++)
+            {
+            Console.WriteLine(firstArray[i]);
+            }
+
+            //Foreach Loop
+
+            Console.WriteLine("Single dimension array with foreach loop");
+            foreach(int n in firstArray)
+            {
+            Console.WriteLine(n);
+            }
+
+            //Multi dimension array
+
+            int[,] secondArray = new int[4, 3] { { 10, 20, 30 }, { 40, 50, 60 }, { 70, 80, 90 }, { 100, 101, 102 } };
+            Console.WriteLine("Multi Dimension array with for loop");
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {   
+                    Console.Write(secondArray[i,j] + "  ");
+                }
+                Console.WriteLine();
+            }
+
+            //jagged array
+
+            int[][] thirdArray = new int[4][];
+            thirdArray[0] = new int[3] {1,2,3 }; //row 0
+            thirdArray[1] = new int[2] {4,5 }; //row 1
+            thirdArray[2] = new int[5] {6,7,8,9,10}; //row 2
+            thirdArray[3] = new int[4] {11,12,13,14 }; //row 3
+            Console.WriteLine("Jagged array:");
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < thirdArray[i].Length; j++)
+                { 
+                    Console.Write(thirdArray[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            //Dictonay
+            Dictionary <string,int> marks = new Dictionary<string, int>()
+            {
+                {"Maths",60 },
+                {"Physics",40 },
+                {"Chemistry",50 }
+            };
+            Console.WriteLine("Physics Marks:");
+            Console.WriteLine(marks["Physics"]);
+
+            //Hashtable
+
+            Hashtable marks2 = new Hashtable()
+            {
+                {"Maths",60 },
+                {"Physics",40 },
+                {"Chemistry",50 },
+                {100,'A' }
+            };
+            Console.WriteLine(marks2[100]);
+            foreach(var item in marks2.Keys)
+            {
+                Console.Write(item + " "); //key
+                Console.WriteLine(marks2[item]); //value
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
