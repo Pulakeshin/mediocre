5caa1dd43f9eb91aa1789dd472abb6be9a707c77
Pulakeshin <54570617+Pulakeshin@users.noreply.github.com>
2019-09-10 18:08:27 +0530

    Add files via upload

---
+++ StringBuilder/StringBuilder/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+namespace StringBuilderExample
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                StringBuilder message = new StringBuilder();
+                message.Append("Welcome ");
+                message.Append("to ");
+                message.Append("Capgemini ");
+                message.Append("Mumbai");
+                string s = message.ToString(); //Convert string builder to string
+                Console.WriteLine(s);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Some Error");
+            }
+            
+
+            Console.ReadKey();
+
+        }
+    }
+}
